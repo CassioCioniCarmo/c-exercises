e59fc4c6bd55129b9b42831c908b1417d205d5f9
CassioCioniCarmo <cassiocioni@hotmail.com>
2024-04-21 16:44:27 -0300

    Added exercise 1

---
+++ exercise_1/exercise_1.c
@@ -0,0 +1,15 @@
+/**
+    Exercise - 1: Hello, World!
+    First C program just to print the phrase "Hello World!"
+    
+    Cassio Cioni Carmo - 04/21/2024
+**/
+
+#include <stdio.h>
+
+int main() 
+{
+    printf("Hello, World!\n");
+    
+    return 0;
+}
